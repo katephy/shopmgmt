ff6956191528f2133df89b153226cab0048fbbd4
katephy <100555136+katephy@users.noreply.github.com>
2022-08-29 17:04:06 -0700

    Add AreaDao

---
+++ src/main/java/com/shop/ecommerce/dao/AreaDao.java
@@ -0,0 +1,10 @@
+package com.shop.ecommerce.dao;
+
+import com.shop.ecommerce.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+
+    List<Area> queryAreaList();
+}
---
+++ src/test/java/com/shop/ecommerce/dao/AreaDaoTest.java
@@ -0,0 +1,26 @@
+package com.shop.ecommerce.dao;
+
+import com.shop.ecommerce.entity.Area;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-dao.xml")
+public class AreaDaoTest {
+
+    @Autowired
+    private AreaDao areaDao;
+
+    @Test
+    public void testQueryAreaList(){
+        List<Area> areaList = areaDao.queryAreaList();
+        Assert.assertEquals(2, areaList.size());
+    }
+
+}
